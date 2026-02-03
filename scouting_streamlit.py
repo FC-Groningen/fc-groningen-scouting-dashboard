0c624d40c2c9f7d371c511984e7e865d31da93b3
Bram <bramvandewater@fcgroningen.nl>
2026-02-03 16:15:06 +0100

    Update scouting_streamlit.py

--- scouting_streamlit.py
+++ scouting_streamlit.py
@@ -856,6 +856,5 @@ grid_response = AgGrid(
     update_mode=GridUpdateMode.SELECTION_CHANGED,
     height=615,
     fit_columns_on_grid_load=False,
-    custom_css=custom_css,
     theme='streamlit'
 )
