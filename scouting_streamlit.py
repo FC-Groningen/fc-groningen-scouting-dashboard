4b7862588b0f0c7cf1c0b461abe64aa00854af5c
Bram <bramvandewater@fcgroningen.nl>
2026-02-03 09:38:15 +0100

    Update scouting_streamlit.py

--- scouting_streamlit.py
+++ scouting_streamlit.py
@@ -496,7 +496,7 @@ st.markdown(
     /* Set sidebar labels and their margin */    
     section[data-testid="stSidebar"] label {{
         margin-bottom: 0.2rem !important;
-        font-size: 16px !important;
+        font-size: 24px !important;
       }}
 
     /* Set margins for dropdowns in the sidebar */  
